5c1f3044eec2dd3b9958ff9c071ab9966c2ba90b
sanzillion <sanzimagery@gmail.com>
2017-05-06 23:36:47 +0800

    Front-End update

--- masterjs.js
+++ masterjs.js
@@ -1,6 +1,23 @@
-
+var value = 0;
 var shit = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='+value;
 
 $(document).ready(function(){
   console.log("FUCKERS!");
+  var button = $("#search");
+  var input = $("#searches");
+  var x = $(".x-mark");
+  x.hide();
+  input.hide();
+  	button.on("click", function(){
+  		input.fadeIn().animate({width: '100%'});
+  		button.fadeOut();
+  		x.fadeIn(3000);
+  		$(".random").css("margin-left", "0px");
+  	});
+  	x.on("click", function(){
+  		x.hide();
+  		input.animate({width: '1%'}).fadeOut();
+  		button.fadeIn();
+  		$(".random").css("margin-left", "5px");
+  	});
 });
